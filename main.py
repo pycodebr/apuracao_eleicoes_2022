d0f3f220a1ada67c4ebeeb4a740df9160dd01286
Felipe Araujo <92059263+imfelipearaujo@users.noreply.github.com>
2022-10-30 15:47:55 -0300

    commit
    atualiza os resultados quando aperta S

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 data = requests.get(
     'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
-    
+
 json_data = json.loads(data.content)
 
 candidato = []
@@ -12,15 +12,25 @@ partido = []
 votos = []
 porcentagem = []
 
-for informacoes in json_data['cand']:
-    
-    if informacoes['seq'] in ['1', '2', '3', '4']:
-        candidato.append(informacoes['nm'])
-        votos.append(informacoes['vap'])
-        porcentagem.append(informacoes['pvap'])
-        
-df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns = [
-    'Candidato', 'Nº de Votos', 'Porcentagem'
-])
+pergunta = ''
 
-print(df_eleicao)
+while pergunta in 'sS':
+    for informacoes in json_data['cand']:
+
+        if informacoes['seq'] in ['1', '2', '3', '4']:
+            candidato.append(informacoes['nm'])
+            votos.append(informacoes['vap'])
+            porcentagem.append(informacoes['pvap'])
+
+    df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
+        'Candidato', 'Nº de Votos', 'Porcentagem'
+    ])
+
+    print(df_eleicao)
+    print()
+    pergunta = str(input('Deseja rever os resultados? [S/N] '))
+    if pergunta in 'Ss':
+        candidato.clear()
+        partido.clear()
+        votos.clear()
+        porcentagem.clear()
